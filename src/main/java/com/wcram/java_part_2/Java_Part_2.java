7ef4a024174f21235639b9bb424ea22bb3ff1b84
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-10-23 18:47:38 -0700

    Initial commit

---
+++ src/main/java/com/wcram/java_part_2/Java_Part_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.wcram.java_part_2;
+
+/**
+ *
+ * @author WCram
+ */
+public class Java_Part_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
